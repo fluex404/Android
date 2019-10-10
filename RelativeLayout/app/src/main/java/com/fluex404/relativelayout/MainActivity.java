3d92fe0623cd40900ef2bc87276546af195bdb78
Isa <isaabqari@gmail.com>
2019-10-10 19:46:17 +0700

    relative layout final learn :D

--- RelativeLayout/app/src/main/java/com/fluex404/relativelayout/MainActivity.java
+++ RelativeLayout/app/src/main/java/com/fluex404/relativelayout/MainActivity.java
@@ -2,13 +2,34 @@ package com.fluex404.relativelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.r1).setOnClickListener(this);
+        findViewById(R.id.r2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.r1:
+                Intent i1 = new Intent(MainActivity.this, RelativeLayout1.class);
+                startActivity(i1);
+                break;
+            case R.id.r2:
+                Intent i2 = new Intent(MainActivity.this, RelativeLayout2.class);
+                startActivity(i2);
+                break;
+            default: break;
+        }
     }
 }
---
+++ RelativeLayout/app/src/main/java/com/fluex404/relativelayout/RelativeLayout3.java
@@ -0,0 +1,14 @@
+package com.fluex404.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayout3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout3);
+    }
+}
