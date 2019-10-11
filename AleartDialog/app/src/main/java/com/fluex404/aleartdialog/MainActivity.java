d2b8331921d3c483bfc6c08e2f58a980b7f8fcdd
Isa <isaabqari@gmail.com>
2019-10-11 21:43:48 +0700

    menambahkan items dan icon pada alert dialog

--- AleartDialog/app/src/main/java/com/fluex404/aleartdialog/MainActivity.java
+++ AleartDialog/app/src/main/java/com/fluex404/aleartdialog/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        findViewById(R.id.food).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showButtonDialog();
+            }
+        });
 
     }
 
@@ -41,6 +50,30 @@ public class MainActivity extends AppCompatActivity {
                     }
                 })
         .setCancelable(false);
+        alert.show();
+    }
+
+    private void showButtonDialog() {
+        final String[] makanan = {"Mie Ayama", "Bakso", "Ayam Goreng", "Nasi Goreng", "Sate"};
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Pilih Menu Makanan")
+                .setIcon(R.drawable.ic_notification)
+                .setItems(makanan, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        switch(i){
+                            case 0:
+                                Toast.makeText(getApplicationContext(), "Anda Memesan "+makanan[0], Toast.LENGTH_SHORT).show();
+                            case 1:
+                                Toast.makeText(getApplicationContext(), "Anda Memesan "+makanan[1], Toast.LENGTH_SHORT).show();
+                            case 2:
+                                Toast.makeText(getApplicationContext(), "Anda Memesan "+makanan[2], Toast.LENGTH_SHORT).show();
+                            case 3:
+                                Toast.makeText(getApplicationContext(), "Anda Memesan "+makanan[3], Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                    }
+                });
         alert.show();
     }
 
